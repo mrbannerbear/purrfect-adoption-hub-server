79d8e3e20740f79561c290ec3c961c337cfe771c
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2023-11-26 09:53:01 +0600

    API for all pets

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 
 const port = process.env.PORT || 4200;
@@ -12,7 +12,6 @@ app.use(express.json()); // Parses incoming json requests
 app.use(cors()); // Allows server to handle incoming requests
 dotenv.config(); // Loads .env file contents into process.env by default
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v41bc23.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,26 +20,49 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
+const db = client.db("pet-adoption0");
+const allPets = db.collection("all-pets");
+
 app.get("/", (req, res) => {
-    res.send("Running Successfully")
-})
+  res.send("Running Successfully");
+});
 
 async function run() {
   try {
+    // General apis
+
+    app.get("/all-pets", async (req, res) => {
+      let query = {};
+
+      const result = await allPets
+        .find(query)
+        .sort({ added_date: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/all-pets/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = { _id: new ObjectId(id) };
+      const result = await allPets.findOne(body);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch(err) {
-    console.log(err)
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
+    console.log(err);
   }
 }
 
 app.listen(port, () => {
-    console.log("Successfully running port", port)
-})
+  console.log("Successfully running port", port);
+});
 
 run().catch(console.dir);
